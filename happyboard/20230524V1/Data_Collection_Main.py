0593d8720a06043ab7f308de7069435c9ff24df9
hsilan-sui <suihsilan1101@icloud.com>
2024-12-25 15:57:20 +0800

    [更動]: 將class ReceivedClawData 獨立為模組 [內容]: 1.基於st7735 error 問題(記憶體分配不足),先將封包相關的class ReceivedClawData獨立為received_claw_data.py 再import進來 2.稍微整理模組
    [依據版本] Based on 2024/12/10_V1.07b_sui

--- happyboard/20230524V1/Data_Collection_Main.py
+++ happyboard/20230524V1/Data_Collection_Main.py
@@ -1,18 +1,23 @@
-VERSION = "V1.07b_sui"
+VERSION = "V1.07b0_sui"
+# test V1.07b0_sui  # 四 機台設定查詢:  拆模組ReceivedClawData&整理模組
 
+#標準庫
 import machine
 import binascii
-from machine import UART
-from umqtt.simple import MQTTClient
-import _thread
-import utime, time
-import network
-import ujson
-from dr.st7735.st7735_4bit import ST7735
-from machine import SPI, Pin
-import gc
-from machine import WDT
 import os
+import utime, time
+import gc
+import _thread
+import ujson
+#外部依賴
+import network
+from machine import UART, Pin, SPI, WDT
+from umqtt.simple import MQTTClient
+#本地
+from received_claw_data import ReceivedClawData
+from dr.st7735.st7735_4bit import ST7735
+
+
 
 #Based on 2023/12/25_V1.07a, Sam 
 # 2024/02/22_V1.07b, Thomas 
@@ -499,75 +504,6 @@ class KindFEILOLIcmd:
     #Clean_Coin_account = 324 ## 清除:投幣帳目
     Ask_Machine_setting = 431
 
-
-class ReceivedClawData:
-    def __init__(self):
-        self.CMD_Verification_code_and_Card_function = 0    # for 一、通訊說明\回覆修改驗證碼、刷卡功能的指令
-        self.Verification_code = bytearray(4)               # for 一、通訊說明\驗證碼
-        self.Machine_Code_number = bytearray(2)             # for 一、通訊說明\機台代號
-        self.Machine_FW_version = bytearray(2)              # for 一、通訊說明\程式版本
-        self.Feedback_Card_function = 0                     # for 一、通訊說明\回覆目前刷卡功能
-
-        self.CMD_Control_Machine = 0                        # for 二、主控制\機台狀態\回覆控制指令 (機台回覆控制代碼)
-        self.Status_of_Current_machine = bytearray(2)       # for 二、主控制\機台狀態\機台目前狀況
-        self.Time_of_Current_game = 0                       # for 二、主控制\機台狀態\當機台目前狀況[0]為0x10=遊戲開始(未控制搖桿)時，回傳的遊戲時間
-        self.Game_amount_of_Player = 0                      # for 二、主控制\機台狀態\玩家遊戲金額(累加金額)
-        self.Way_of_Starting_game = 0                       # for 二、主控制\機台狀態\遊戲啟動方式
-        self.Cumulation_amount_of_Sale_card = 0             # for 二、主控制\機台狀態\售價小卡顯示用累加金額
-
-        self.Payment_amount_of_This_order = 0               # for 二、主控制\傳送交易資料\此次扣款金額
-        self.Number_of_Original_games_to_Start = 0          # for 二、主控制\傳送交易資料\啟動原局數
-        self.Number_of_Gift_games_to_Start = 0              # for 二、主控制\傳送交易資料\啟動贈局數
-        self.Number_dollars_of_Per_game = 0                 # for 二、主控制\傳送交易資料\每局幾元
-        self.Time_of_Payment_countdown_Or_fail = 0          # for 二、主控制\等待刷卡倒數/交易失敗\IPC倒數時間or失敗
-        self.CMD_Mode_of_Payment = 0                        # for 二、主控制\回覆遊戲啟動方式(01=電子支付)
-
-        # 未定義                                            # for 二、主控制\套餐設定、參數回報
-
-        self.Number_of_Original_Payment = 0     # for 三、帳目查詢\遠端帳目\悠遊卡支付次數
-        self.Number_of_Gift_Payment = 0         # for 三、帳目查詢\遠端帳目\悠遊卡贈送次數
-        self.Number_of_Coin = 0                 # for 三、帳目查詢\遠端帳目\投幣次數
-        self.Number_of_Award = 0                # for 三、帳目查詢\遠端帳目、投幣帳目\禮品出獎次數
-        self.Bank_of_Award_rate = 0             # for 三、帳目查詢\投幣帳目\中獎率銀行
-        self.Number_of_Total_games = 0          # for 三、帳目查詢\投幣帳目\總遊戲次數
-        # 以下 四、五、六都還沒檢查、還沒逐一順過
-        '''
-        self.CMD_Item_of_Machine_Setting = 0        # for 四、機台設定查詢
-        self.Time_of_game = 0                       # for 四、機台設定查詢\基本設定A
-        self.Time_of_Keeping_cumulation = 0         # for 四、機台設定查詢\基本設定A
-        self.Time_of_Show_music = 0                 # for 四、機台設定查詢\基本設定A
-        self.Enable_of_Midair_Grip = 0              # for 四、機台設定查詢\基本設定A
-        self.Amount_of_Award = 0                    # for 四、機台設定查詢\基本設定A
-        self.Amount_of_Present_cumulation = 0       # for 四、機台設定查詢\基本設定A
-        self.Delay_of_Push_talon = 0                # for 四、機台設定查詢\基本設定B
-        self.Delay_of_Suspend_pulled_talon = 0      # for 四、機台設定查詢\基本設定B
-        self.Enable_random_of_Pushing_talon = 0     # for 四、機台設定查詢\基本設定B
-        self.Enable_random_of_Clamping = 0          # for 四、機台設定查詢\基本設定B
-        self.Time_of_Push_talon = 0                 # for 四、機台設定查詢\基本設定B
-        self.Time_of_Suspend_and_Pull_talon = 0     # for 四、機台設定查詢\基本
-
-        self.Delay_of_Pull_talon = 0                            # for 四、機台設定查詢\基本設定B
-        self.Enable_of_Sales_promotion = 0                      # for 四、機台設定查詢\基本設定C
-        self.Which_number_starting_when_Sales_promotion = 0     # for 四、機台設定查詢\基本設定C
-        self.Number_of_Strong_grip_when_Sales_promotion = 0     # for 四、機台設定查詢\基本設定C
-        self.Value_of_Hi_voltage = 0                    # for 四、機台設定查詢\抓力電壓
-        self.Value_of_Mid_voltage = 0                   # for 四、機台設定查詢\抓力電壓
-        self.Value_of_Lo_voltage = 0                    # for 四、機台設定查詢\抓力電壓
-        self.Distance_of_Mid_voltage_and_Top = 0        # for 四、機台設定查詢\抓力電壓
-        self.Hi_voltage_of_Guaranteed_prize = 0         # for 四、機台設定查詢\抓力電壓
-        self.Speed_of_Moving_forward = 0                # for 四、機台設定查詢\馬達速度
-        self.Speed_of_Moving_back = 0                   # for 四、機台設定查詢\馬達速度
-        self.Speed_of_Moving_left = 0                   # for 四、機台設定查詢\馬達速度
-        self.Speed_of_Moving_right = 0                  # for 四、機台設定查詢\馬達速度
-        self.Speed_of_Moving_up = 0                     # for 四、機台設定查詢\馬達速度
-        self.Speed_of_Moving_down = 0                   # for 四、機台設定查詢\馬達速度
-        self.RPM_of_All_horizontal_sides = 0            # for 四、機台設定查詢\馬達速度
-        self.CMD_State_of_Display = 0           # for 五、悠遊卡功能\維修顯示
-        self.X_Value_of_02_State = 0            # for 五、悠遊卡功能\維修顯示
-        self.CMD_Backstage_function = 0         # for 五、悠遊卡功能\後台功能
-        self.Error_Code_of_IPC_Feedback = 0     # for 五、悠遊卡功能\後台功能
-        '''
-        self.Error_Code_of_Machine = 99          # for 六、 機台故障代碼表
 
 # 发送封包給娃娃機的副程式
 FEILOLI_packet_id = 0
