4074c875e3f58e50177c8ffa23ecc07a51cebf20
hsilan-sui <suihsilan1101@icloud.com>
2025-03-05 14:37:37 +0800

    add happyboard/20250305_VO1_01a_debug version

--- happyboard/20250305_VO1_01a_debug/main.py
+++ happyboard/20250305_VO1_01a_debug/main.py
@@ -191,7 +191,7 @@ if filename in file_list:
           user="hsilan-sui",  # Required
           repo="happycollector",  # Required
           branch="Sui_Branch",  # Optional: Defaults to "master"
-          working_dir="happyboard/20250227_VO1_01a_light",  # Optional: Defaults to "app"
+          working_dir="happyboard/20250305_VO1_01a_debug",  # Optional: Defaults to "app"
           # "happyboard/20230524V1"
           files=file_list
       )
